50ffe9d8c546a4ff5f14f5ef6c0d118e6c501a1c
MercyMilkahNjokiKahungu <mercy.njoki@student.moringaschool.com>
2022-08-03 21:27:49 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ function introduction(name){
     return `Hi, my name is ${name}.`;
 }
 introduction('Aki');
+introduction('Samip');
 
 function introductionWithLanguage(name, language){
     return `Hi, my name is ${name} and I am learning to program in ${language}.`;
 }
-introductionWithLanguage('Aki, Python');
+introductionWithLanguage('Aki', 'Ember.js');
+introductionWithLanguage('Aki', 'React');
 
 function introductionWithLanguageOptional(name, language = "JavaScript"){
     return `Hi, my name is ${name} and I am learning to program in ${language}.`;
 }
-introductionWithLanguageOptional();
+introductionWithLanguageOptional('Gracie');
